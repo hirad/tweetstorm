13d451153ce7f2a03cc434253c3d93e31709f587
Hirad Motamed <hirad@pendarlabs.com>
2017-09-26 09:50:13 -0700

    initial commit

---
+++ stormgen.hs
@@ -0,0 +1,64 @@
+import Data.List
+import Data.Maybe
+
+data Tweet
+  = Tweet {
+      content :: String
+    , index :: Int
+    }
+    deriving (Eq)
+
+showTweet :: Tweet -> String
+showTweet t = show $ (show $ index t) ++ "/ " ++ content t
+
+instance Ord Tweet where
+  (Tweet _ i1) `compare` (Tweet _ i2) = i1 `compare` i2
+
+instance Show Tweet where
+  show = showTweet
+
+data State
+  = State {
+      currentIndex :: Int
+    , currentWords :: [String]
+    , currentLength :: Int
+    , accTweets :: [Tweet]
+    }
+    deriving (Show)
+
+-- stormGen :: String -> [Tweet]
+-- -- Here I can use Data.List.Split's chunksOf function to generate
+-- -- chunks of the right size, but I need a way to get the right size.
+-- stormGen s = makeNextTweet 1 s []
+--   where makeNextTweet
+
+step :: State -> String -> State
+step state str =
+  let indexLength = (length $ show $ currentIndex state) + 2
+      wordLength = currentLength state + length str
+      spaceLength = pred $ length $ currentWords state
+      totalLength = indexLength + wordLength + spaceLength
+  in
+    if totalLength > 140 then
+      State { currentIndex = currentIndex state + 1
+            , currentWords = [str]
+            , currentLength = 0
+            , accTweets =
+                accTweets state
+                ++ [Tweet { content = unwords $ currentWords state
+                          ,  index = currentIndex state
+                          }]
+            }
+    else
+      state {
+              currentWords = currentWords state ++ [str]
+            , currentLength = currentLength state + length str
+            }
+
+emptyState = State { currentIndex = 1, currentWords = [], currentLength = 0, accTweets = [] }
+
+tweetStorm = allTweetsFrom . foldl' step emptyState . words
+  where allTweetsFrom s = accTweets s ++ (maybeToList $ lastTweet (currentIndex s) (currentWords s))
+        lastTweet _ [] = Nothing
+        lastTweet idx wrds = Just Tweet { content = unwords wrds, index = idx }
+
